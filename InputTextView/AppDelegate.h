f25e9fa23a64a09f41b33b125ea71f355f163f18
袁鑫亮 <306619887@qq.com>
2018-11-27 14:02:45 +0800

    Initial Commit

---
+++ InputTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InputTextView
+//
+//  Created by XL Yuen on 2018/11/27.
+//  Copyright © 2018 XL Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InputTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InputTextView
+//
+//  Created by XL Yuen on 2018/11/27.
+//  Copyright © 2018 XL Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
